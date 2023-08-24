0ff3f6457556f83c6616fdd7a8e16091b50fe9c6
Kamol-Paul <s1910176121@ru.ac.bd>
2023-08-25 03:47:51 +0600

    work somehow

---
+++ chobichokro-api/src/main/java/com/chobichokro/api/models/JwtResponse.java
@@ -0,0 +1,16 @@
+package com.chobichokro.api.models;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class JwtResponse {
+    private String jwtToken;
+    private String username;
+
+}
