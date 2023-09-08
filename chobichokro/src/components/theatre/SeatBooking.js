680b05c3886da755c85e4ee209f6b51561a59973
Md. Khalid Hasan <shohag9438@gmail.com>
2023-09-08 19:19:22 +0600

    ticket ui added

--- chobichokro/src/components/theatre/SeatBooking.js
+++ chobichokro/src/components/theatre/SeatBooking.js
@@ -1,174 +1,131 @@
-import React from "react";
-import { Button } from "react-bootstrap";
-import './SeatBooking.css'
-import { Card } from "react-bootstrap";
-import { CardTitle } from "reactstrap";
-function SeatBooking() {
-    return(
-        <div>
-            <Card style={{backgroundColor: 'lightblue', marginLeft: '250px', borderRadius: '7px'}}>
-                <Card.Header>
-                    Tickets
-                </Card.Header>
-                <Card.Body>
-                <ul style={{display: 'flex', marginBottom: '10px'}}>
-                <li><button>A01</button></li>
-                <li><button>A02</button></li>
-                <li><button>A03</button></li>
-                <li><button>A04</button></li>
-                <li><button>A05</button></li>
-                <li><button>A06</button></li>
-                <li><button>A07</button></li>
-                <li><button>A08</button></li>
-                <li><button>A09</button></li>
-                <li><button>A10</button></li>
-                <li><button>A11</button></li>
-                <li><button>A12</button></li>
-                <li><button>A13</button></li>
-                <li><button>A14</button></li>
-                <li><button>A15</button></li>
-                <li><button>A16</button></li>
-                <li><button>A17</button></li>
-                <li><button>A18</button></li>
-                <li><button>A19</button></li>
-                <li><button>A20</button></li>
-            </ul>
-            <ul style={{display: 'flex', marginBottom: '10px'}}>
-                <li><button>B01</button></li>
-                <li><button>B02</button></li>
-                <li><button>B03</button></li>
-                <li><button>B04</button></li>
-                <li><button>B05</button></li>
-                <li><button>B06</button></li>
-                <li><button>B07</button></li>
-                <li><button>B08</button></li>
-                <li><button>B09</button></li>
-                <li><button>B10</button></li>
-                <li><button>B11</button></li>
-                <li><button>B12</button></li>
-                <li><button>B13</button></li>
-                <li><button>B14</button></li>
-                <li><button>B15</button></li>
-                <li><button>B16</button></li>
-                <li><button>B17</button></li>
-                <li><button>B18</button></li>
-                <li><button>B19</button></li>
-                <li><button>B20</button></li>
-            </ul>
-            <ul style={{display: 'flex', marginBottom: '10px'}}>
-                <li><button>C01</button></li>
-                <li><button>C02</button></li>
-                <li><button>C03</button></li>
-                <li><button>C04</button></li>
-                <li><button>C05</button></li>
-                <li><button>C06</button></li>
-                <li><button>C07</button></li>
-                <li><button>C08</button></li>
-                <li><button>C09</button></li>
-                <li><button>C10</button></li>
-                <li><button>C11</button></li>
-                <li><button>C12</button></li>
-                <li><button>C13</button></li>
-                <li><button>C14</button></li>
-                <li><button>C15</button></li>
-                <li><button>C16</button></li>
-                <li><button>C17</button></li>
-                <li><button>C18</button></li>
-                <li><button>C19</button></li>
-                <li><button>C20</button></li>
-            </ul>
-            <ul style={{display: 'flex', marginBottom: '10px'}}>
-                <li><button>D01</button></li>
-                <li><button>D02</button></li>
-                <li><button>D03</button></li>
-                <li><button>D04</button></li>
-                <li><button>D05</button></li>
-                <li><button>D06</button></li>
-                <li><button>D07</button></li>
-                <li><button>D08</button></li>
-                <li><button>D09</button></li>
-                <li><button>D10</button></li>
-                <li><button>D11</button></li>
-                <li><button>D12</button></li>
-                <li><button>D13</button></li>
-                <li><button>D14</button></li>
-                <li><button>D15</button></li>
-                <li><button>D16</button></li>
-                <li><button>D17</button></li>
-                <li><button>D18</button></li>
-                <li><button>D19</button></li>
-                <li><button>D20</button></li>
-            </ul>
-            <ul style={{display: 'flex', marginBottom: '10px'}}>
-                <li><button>E01</button></li>
-                <li><button>E02</button></li>
-                <li><button>E03</button></li>
-                <li><button>E04</button></li>
-                <li><button>E05</button></li>
-                <li><button>E06</button></li>
-                <li><button>E07</button></li>
-                <li><button>E08</button></li>
-                <li><button>E09</button></li>
-                <li><button>E10</button></li>
-                <li><button>E11</button></li>
-                <li><button>E12</button></li>
-                <li><button>E13</button></li>
-                <li><button>E14</button></li>
-                <li><button>E15</button></li>
-                <li><button>E16</button></li>
-                <li><button>E17</button></li>
-                <li><button>E18</button></li>
-                <li><button>E19</button></li>
-                <li><button>E20</button></li>
-            </ul>
-            <ul style={{display: 'flex', marginBottom: '10px'}}>
-                <li><button>F01</button></li>
-                <li><button>F02</button></li>
-                <li><button>F03</button></li>
-                <li><button>F04</button></li>
-                <li><button>F05</button></li>
-                <li><button>F06</button></li>
-                <li><button>F07</button></li>
-                <li><button>F08</button></li>
-                <li><button>F09</button></li>
-                <li><button>F10</button></li>
-                <li><button>F11</button></li>
-                <li><button>F12</button></li>
-                <li><button>F13</button></li>
-                <li><button>F14</button></li>
-                <li><button>F15</button></li>
-                <li><button>F16</button></li>
-                <li><button>F17</button></li>
-                <li><button>F18</button></li>
-                <li><button>F19</button></li>
-                <li><button>F20</button></li>
-            </ul>
-            <ul style={{display: 'flex', marginBottom: '10px'}}>
-                <li><button>G01</button></li>
-                <li><button>G02</button></li>
-                <li><button>G03</button></li>
-                <li><button>G04</button></li>
-                <li><button>G05</button></li>
-                <li><button>G06</button></li>
-                <li><button>G07</button></li>
-                <li><button>G08</button></li>
-                <li><button>G09</button></li>
-                <li><button>G10</button></li>
-                <li><button>G11</button></li>
-                <li><button>G12</button></li>
-                <li><button>G13</button></li>
-                <li><button>G14</button></li>
-                <li><button>G15</button></li>
-                <li><button>G16</button></li>
-                <li><button>G17</button></li>
-                <li><button>G18</button></li>
-                <li><button>G19</button></li>
-                <li><button>G20</button></li>
-            </ul>
-                </Card.Body>
-            </Card>
-        </div>
-    );
-}
+import React from 'react'
+import './SeatBooking.css';
+import { Card } from 'react-bootstrap';
+import { CardBody, CardFooter, CardHeader } from 'reactstrap';
 
-export default SeatBooking;
+class SeatBooking extends React.Component {
+ 
+    constructor() {
+      super();
+        this.state = {
+        seat: [
+          ['A01','A02','A03', 'A04', 'A05', 'A06', 'A07', 'A08', 'A09', 'A10', 'A11','A12','A13', 'A14', 'A15', 'A16', 'A17', 'A18', 'A19', 'A20'],
+          ['B01','B02','B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B09', 'B10', 'B11','B12','B13', 'B14', 'B15', 'B16', 'B17', 'B18', 'B19', 'B20'],
+          ['C01','C02','C03', 'C04', 'C05', 'C06', 'C07', 'C08', 'C09', 'C10', 'C11','C12','C13', 'C14', 'C15', 'C16', 'C17', 'C18', 'C19', 'C20'],
+          ['D01','D02','D03', 'D04', 'D05', 'D06', 'D07', 'D08', 'D09', 'D10', 'D11','D12','D13', 'D14', 'D15', 'D16', 'D17', 'D18', 'D19', 'D20'],
+          ['E01','E02','E03', 'E04', 'E05', 'E06', 'E07', 'E08', 'E09', 'E10', 'E11','E12','E13', 'E14', 'E15', 'E16', 'E17', 'E18', 'E19', 'E20'],
+          ['F01','F02','F03', 'F04', 'F05', 'F06', 'F07', 'F08', 'F09', 'F10', 'F11','F12','F13', 'F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'F20'],
+          ['G01','G02','G03', 'G04', 'G05', 'G06', 'G07', 'G08', 'G09', 'G10', 'G11','G12','G13', 'G14', 'G15', 'G16', 'G17', 'G18', 'G19', 'G20'],
+        ],
+        seatAvailable: [
+            ['A01','A02','A03', 'A04', 'A05', 'A06', 'A07', 'A08', 'A09', 'A10', 'A11','A12','A13', 'A14', 'A15', 'A16', 'A17', 'A18', 'A19', 'A20'],
+            ['B01','B02','B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B09', 'B10', 'B11','B12','B13', 'B14', 'B15', 'B16', 'B17', 'B18', 'B19', 'B20'],
+            ['C01','C02','C03', 'C04', 'C05', 'C06', 'C07', 'C08', 'C09', 'C10', 'C11','C12','C13', 'C14', 'C15', 'C16', 'C17', 'C18', 'C19', 'C20'],
+            ['D01','D02','D03', 'D04', 'D05', 'D06', 'D07', 'D08', 'D09', 'D10', 'D11','D12','D13', 'D14', 'D15', 'D16', 'D17', 'D18', 'D19', 'D20'],
+            ['E01','E02','E03', 'E04', 'E05', 'E06', 'E07', 'E08', 'E09', 'E10', 'E11','E12','E13', 'E14', 'E15', 'E16', 'E17', 'E18', 'E19', 'E20'],
+            ['F01','F02','F03', 'F04', 'F05', 'F06', 'F07', 'F08', 'F09', 'F10', 'F11','F12','F13', 'F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'F20'],
+            ['G01','G02','G03', 'G04', 'G05', 'G06', 'G07', 'G08', 'G09', 'G10', 'G11','G12','G13', 'G14', 'G15', 'G16', 'G17', 'G18', 'G19', 'G20'],
+        ],
+        seatReserved: [],
+        seatUnavailable: ['A12', 'B13', 'C14'],
+        reservationCount: 0,
+      }
+    }
+    
+    handleReservationSubmit = () => {
+        // Log the reservationCount value to the console
+        console.log(`Total Reservations: ${this.state.reservationCount}`);
+        return <h1>nbxvnkbbksd</h1>
+      };
+    
+    onClickData(seat) {
+      if(this.state.seatReserved.indexOf(seat) > -1 ) {
+        this.setState({
+          seatAvailable: this.state.seatAvailable.concat(seat),
+          seatReserved: this.state.seatReserved.filter(res => res !== seat),
+          reservationCount: this.state.reservationCount - 1,
+          
+        })
+      } else {
+        this.setState({
+          seatReserved: this.state.seatReserved.concat(seat),
+          seatAvailable: this.state.seatAvailable.filter(res => res !== seat),
+          reservationCount: this.state.reservationCount + 1,
+        })
+      }
+    }
+    
+    render() {
+      return (
+        <div style={{marginLeft: '120px'}}>
+          <Card style={{backgroundColor: 'gray', borderRadius: '5px'}}>
+            <CardHeader>
+                <label>
+                    <input type='radio' style={{backgroundColor: 'aqua'}}>
+                    </input>
+                    Available
+                </label>
+                <label style={{marginLeft: '20px'}}>
+                    <input type='radio' style={{backgroundColor: 'lightcoral'}}>
+                    </input>
+                    Booked
+                </label>
+                <h4 style={{color: 'black'}}>Total selected Seats: {this.state.reservationCount}</h4>
+                
+            </CardHeader>
+            <CardBody>
+                <DrawGrid 
+                seat = { this.state.seat }
+                available = { this.state.seatAvailable }
+                reserved = { this.state.seatReserved }
+                unavailable = {this.state.seatUnavailable}
+                onClickData = { this.onClickData.bind(this) }
+                />
+            </CardBody>
+            <CardFooter>
+                <button style={{marginLeft: '550px', cursor: 'pointer', backgroundColor: 'gold', borderColor: 'gold'}}
+                onClick={this.handleReservationSubmit}>Submit</button>
+                
+            </CardFooter>
+          </Card>
+        </div>
+      )
+    }
+    
+      
+  }
+
+  export default SeatBooking
+  
+  class DrawGrid extends React.Component {
+
+    render() {
+      return (
+         <div className="container">
+          <table className="grid">
+            <tbody>
+              { this.props.seat.map((numList,i) => (
+                <tr key={i}>
+                { numList.map ( seat_no =>
+                  <td 
+                    className={this.props.unavailable.indexOf(seat_no) > -1? 'unavailable': this.props.reserved.indexOf(seat_no) > -1? 'reserved': 'available'}
+                    key={seat_no} onClick = {e => this.onClickSeat(seat_no)}>{seat_no} 
+                  </td>
+                )}
+                </tr>
+              ))
+              }
+            </tbody>
+          </table>
+          
+          {/* <AvailableList available = { this.props.available } />
+          <ReservedList reserved = { this.props.reserved } /> */}
+         </div>
+      )
+    }
+    
+    
+    onClickSeat(seat) {
+      this.props.onClickData(seat);
+    }
+  }
