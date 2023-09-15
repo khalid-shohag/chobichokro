7808da0701b7ba19ff2a6ac8acd5813b55a84bf2
Md. Khalid Hasan <shohag9438@gmail.com>
2023-09-15 22:07:56 +0600

    Login credential check to visit the page

--- chobichokro/src/components/theatre/TheatrePage.js
+++ chobichokro/src/components/theatre/TheatrePage.js
@@ -8,9 +8,22 @@ import SeatBooking from "./SeatBooking";
 import NewShow from "./show/NewShow";
 import ShowList from "./show/ShowList";
 import { RunningShow } from "./show/RunningShow";
+import { useLocation } from "react-router-dom";
+import Login from "../Login";
+
 function TheatrePage() {
 
     const [ticket, setTicket] = useState(false);
+    const location = useLocation();
+    
+    if (localStorage.getItem('token')=='') {
+        
+        const accessToken = location.state?.token || '';
+    
+        localStorage.setItem('token', accessToken); 
+    }
+    const isAuthenticated = !!localStorage.getItem('token');
+
     const [show, setShow] = useState(false);
     const [runningShow, setRunningShow] = useState(true);
     const handleTicket = () => {
@@ -29,85 +42,101 @@ function TheatrePage() {
         setRunningShow(true);
     }
 
-  return (
-    <div className="container">
-      <div className="row">
-        {/* <div className="column">Row 1 - Column 1</div> */}
-        {/* <div className="column">Row 1 - Column 2</div> */}
-        <Navbar />
-      </div>
-      <div className="row" style={{marginTop: '100px'}}>
-        <div className="column">
-            <Button className="btn" onClick={handleRunningShow}>
-                <Card>
-                    <Card.Body>
-                        Running
-                    </Card.Body>
-                </Card>
-            </Button>
+  if (!isAuthenticated)
+    return(
+        <div>
+            <div>
+            <Navbar />
+            </div>
+            <div style={{marginTop: '60px'}}>
+            <Login value={'theatre'} />
+            </div>
+            
         </div>
-        <div className="column">
-            <Button className="btn"
-            onClick={handleShow}>
-                <Card>
-                    <Card.Body>
-                        Add Show
-                    </Card.Body>
-                </Card>
-            </Button>
-        </div>
-        <div className="column">
-            <Button className="btn"
-            onClick={handleTicket}>
-                <Card>
-                    <Card.Body>
-                        Ticket
-                    </Card.Body>
-                </Card>
-            </Button>
-        </div>
-        <div className="column">
-            <Button className="btn">
-                <Card>
-                    <Card.Body>
-                        Upcoming
-                    </Card.Body>
-                </Card>
-            </Button>
-        </div>
-        <div className="column">
-            <Button className="btn">
-                <Card>
-                    <Card.Body>
-                        Reel
-                    </Card.Body>
-                </Card>
-            </Button>
-        </div>
-        {/* <div className="column">Row 2 - Column 2</div> */}
-      </div>
-      {
-        (ticket &&
-         <div className="row">
-            <SeatBooking />
-        </div>)
-        
-      }
-      {
-        (show &&
+    );
+
+    else
+        return (
+            <div className="container">
             <div className="row">
-               <NewShow />
-           </div>)
-      }
-      {
-        (runningShow &&
-            <div className="row">
-               <RunningShow />
-           </div>)
-      }
-      
-    </div>
-  );
+                {/* <div className="column">Row 1 - Column 1</div> */}
+                {/* <div className="column">Row 1 - Column 2</div> */}
+                <Navbar />
+            </div>
+            <div className="row" style={{marginTop: '100px'}}>
+                <div className="column">
+                    <Button className="btn" onClick={handleRunningShow}>
+                        <Card>
+                            <Card.Body>
+                                Running
+                            </Card.Body>
+                        </Card>
+                    </Button>
+                </div>
+                <div className="column">
+                    <Button className="btn"
+                    onClick={handleShow}>
+                        <Card>
+                            <Card.Body>
+                                Add Show
+                            </Card.Body>
+                        </Card>
+                    </Button>
+                </div>
+                <div className="column">
+                    <Button className="btn"
+                    onClick={handleTicket}>
+                        <Card>
+                            <Card.Body>
+                                Ticket
+                            </Card.Body>
+                        </Card>
+                    </Button>
+                </div>
+                <div className="column">
+                    <Button className="btn">
+                        <Card>
+                            <Card.Body>
+                                Upcoming
+                            </Card.Body>
+                        </Card>
+                    </Button>
+                </div>
+                <div className="column">
+                    <Button className="btn">
+                        <Card>
+                            <Card.Body>
+                                Reel
+                            </Card.Body>
+                        </Card>
+                    </Button>
+                </div>
+                {/* <div className="column">Row 2 - Column 2</div> */}
+            </div>
+            {
+                (ticket &&
+                <div className="row">
+                    <SeatBooking />
+                </div>)
+                
+            }
+            {
+                (show &&
+                    <div className="row">
+                    <NewShow />
+                </div>)
+            }
+            {
+                (runningShow &&
+                    <div className="row">
+                    <RunningShow />
+                </div>)
+            }
+            
+            </div>
+        );
+   
+   
 }
 
 
