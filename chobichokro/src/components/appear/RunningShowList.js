b1bbc0f00bc5e4128e35dff1c7e93aef8915308b
Md. Khalid Hasan <shohag9438@gmail.com>
2023-09-26 04:52:49 +0600

    ui changed

--- chobichokro/src/components/appear/RunningShowList.js
+++ chobichokro/src/components/appear/RunningShowList.js
@@ -41,7 +41,7 @@ function RunningShowList() {
                 <h1 style={{marginLeft: '38%'}}>Running Show- {name} </h1>
                 {theatreList.map((rv) => {
                     return(
-                        <Card key={rv.id} style={{ marginLeft: '35%', padding: '10px', fontStyle: 'italic', fontSize: '18px', marginBottom: '30px', borderRadius: '5px', height: 'auto', width: '35%', backgroundColor: 'lavender'}}>
+                        <Card key={rv.id} style={{ marginLeft: '35%', padding: '10px', fontStyle: 'italic', fontSize: '18px', marginBottom: '30px', marginTop: '2%', boxShadow: '0 0 10px black', borderRadius: '5px', height: 'auto', width: '35%', backgroundColor: 'lightcyan'}}>
                             <CardBody style={{fontSize: '25px', marginLeft: '32%'}}>
                                 <h6>{rv.name}</h6>
                                 {rv.address}
